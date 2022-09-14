8793805f5f328e8226bf3b282061553c1a2a32a8
baonq888 <109365293+baonq888@users.noreply.github.com>
2022-09-14 15:36:25 +0700

    Add files via upload

---
+++ Yelp_clone/src/main/java/com/bds/Yelp_clone/YelpCloneApplication.java
@@ -0,0 +1,13 @@
+package com.bds.Yelp_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpCloneApplication.class, args);
+	}
+
+}
---
+++ Yelp_clone/src/test/java/com/bds/Yelp_clone/YelpCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bds.Yelp_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
